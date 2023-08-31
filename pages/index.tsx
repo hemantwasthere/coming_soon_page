06ac480a74290cdb71b4917a6a6fa59df791e2ca
Hemant <hrsmart2150@gmail.com>
2023-08-31 19:26:12 +0530

    added coming soon page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,17 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div class="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4">
-      <div
-        class="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30"
-        style="background-image: url('https://images.unsplash.com/photo-1604093882750-3ed498f3178b');"
-      ></div>
-      <h1 class="text-5xl md:text-7xl text-white font-bold mb-8 z-10">
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4">
+      <div className="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30 bg-[url('https://images.unsplash.com/photo-1604093882750-3ed498f3178b')]"></div>
+      <h1 className="text-5xl md:text-7xl text-white font-bold mb-8 z-10">
         Coming Soon
       </h1>
-      <p class="text-white text-xl md:text-2xl">
-        We're working hard to bring you something amazing. Stay tuned!
+      <p className="text-white text-xl md:text-2xl">
+        We&rsquo;re working hard to bring you something amazing. Stay tuned!
       </p>
     </div>
   );
