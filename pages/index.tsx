2dcbd6827c200e05b677ebd52adf1fcd30b5d6c5
Hemant <hrsmart2150@gmail.com>
2023-08-31 19:54:32 +0530

    Made headlines center

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4">
       <div className="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30 bg-[url('https://images.unsplash.com/photo-1604093882750-3ed498f3178b')]"></div>
-      <h1 className="text-5xl md:text-7xl text-white font-bold mb-8 z-10">
+      <h1 className="text-5xl md:text-7xl text-white font-bold mb-8 z-10 text-center">
         Coming Soon
       </h1>
-      <p className="text-white text-xl md:text-2xl">
+      <p className="text-white text-center text-xl md:text-2xl">
         We&rsquo;re working hard to bring you something amazing. Stay tuned!
       </p>
     </div>
